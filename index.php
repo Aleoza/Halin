0a8c7c5a1d7c50420004145a6409ddba4ae6ce0d
Aleoza <alovmeleyi@gmail.com>
2019-05-17 17:09:17 +0300

    index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content= "width=device-width, initial-scale=1.0">
+	<title>Portal</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style3.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
+<body>
+
+	<div class="row">
+		<div class="col s12 l2 purple"></div>
+		<div class="col s12 l8 center-align">
+
+      <div class="card">
+        <div class="card-image">
+          <img src="img/1.jpg" style="height: 150px">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+
+
+
+
+			<div class="card intro">
+				<img src="img/pr.jpg" class="over circle">
+				<h4 class="baslik">Kahve Dükkani</h4>
+				<h6 class="descrip">Bu wi-fi ağını kullanmak için giriş yapmalısınız.</h6>
+			</div>
+		</div>
+		<div class="col s12 l2 blue"></div>
+	</div>
+
+</body>
+</html>
